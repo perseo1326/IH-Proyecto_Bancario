fa811230f113ac26d3df8605b4c1a42befc98e89
John W <perseo1326@gmail.com>
2022-10-21 21:40:43 +0200

    creacion de proyecto y configuracion de DB

---
+++ src/main/java/com/josko/proyecto_bancario/ProyectoBancarioApplication.java
@@ -0,0 +1,13 @@
+package com.josko.proyecto_bancario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBancarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoBancarioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/josko/proyecto_bancario/ProyectoBancarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josko.proyecto_bancario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoBancarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
