20a74e7ea89864b897888ec12718d6376d9e4028
John W <perseo1326@gmail.com>
2022-10-22 19:52:53 +0200

    Creacion de entidad 'Address' #7

---
+++ src/main/java/com/josko/proyecto_bancario/models/AccountHolder.java
@@ -0,0 +1,14 @@
+package com.josko.proyecto_bancario.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "AccountHolders")
+public class AccountHolder extends User {
+
+
+
+
+
+}
---
+++ src/main/java/com/josko/proyecto_bancario/models/Address.java
@@ -0,0 +1,114 @@
+package com.josko.proyecto_bancario.models;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "addresses")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "address_id", nullable = false)
+    private Long id;
+
+    private String country;
+    private String city;
+    private String street;
+    private String houseNumber;
+    private String homeUnit;
+    private String comment;
+
+    public Address() {
+    }
+
+    public Address(String country, String city, String street, String houseNumber, String homeUnit, String comment) {
+        this.country = country;
+        this.city = city;
+        this.street = street;
+        this.houseNumber = houseNumber;
+        this.homeUnit = homeUnit;
+        this.comment = comment;
+    }
+
+    @Override
+    public String toString() {
+        return "\nAddress{" +
+                "id=" + id +
+                ", country='" + country + '\'' +
+                ", city='" + city + '\'' +
+                ", street='" + street + '\'' +
+                ", houseNumber='" + houseNumber + '\'' +
+                ", homeUnit='" + homeUnit + '\'' +
+                ", comment='" + comment + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return getId().equals(address.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public String getHomeUnit() {
+        return homeUnit;
+    }
+
+    public void setHomeUnit(String homeUnit) {
+        this.homeUnit = homeUnit;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
