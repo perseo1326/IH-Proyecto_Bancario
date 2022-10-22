1593389d47969a8127d30338eedeb25bd05c57fb
John W <perseo1326@gmail.com>
2022-10-22 19:33:43 +0200

    creacion de entidad 'Admin' #4

---
+++ src/main/java/com/josko/proyecto_bancario/models/Admin.java
@@ -0,0 +1,21 @@
+package com.josko.proyecto_bancario.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Admin")
+public class Admin extends User {
+
+    public Admin() {
+    }
+
+    public Admin(String name) {
+        super(name);
+    }
+
+    @Override
+    public String toString() {
+        return "\nAdmin{ " + super.toString() + " }";
+    }
+}
