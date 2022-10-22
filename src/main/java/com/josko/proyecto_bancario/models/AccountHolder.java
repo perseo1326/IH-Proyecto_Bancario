34826e6da8fa90da45621aa24ab5b4e95ed76063
John W <perseo1326@gmail.com>
2022-10-22 22:39:59 +0200

    Creacion Entidad 'Account holder' #6

--- src/main/java/com/josko/proyecto_bancario/models/AccountHolder.java
+++ src/main/java/com/josko/proyecto_bancario/models/AccountHolder.java
@@ -1,14 +1,68 @@
 package com.josko.proyecto_bancario.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "AccountHolders")
 public class AccountHolder extends User {
 
 
+    private LocalDate birthDate;
 
+    @OneToOne
+    @JoinColumn(name = "address_address_id")
+    private Address address;
+
+    @OneToOne
+    @JoinColumn(name = "mailing_address_address_id")
+    private Address mailingAddress;
+
+    public AccountHolder() {
+    }
+
+    public AccountHolder(String name, LocalDate birthDate, Address address, Address mailingAddress) {
+        super(name);
+        this.birthDate = birthDate;
+        this.address = address;
+        this.mailingAddress = mailingAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "\nAccountHolder{" + super.toString() +
+                "birthDate=" + birthDate +
+                ", address=" + address +
+                ", mailingAddress=" + mailingAddress +
+                '}';
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public void setMailingAddress(Address mailingAddress) {
+        this.mailingAddress = mailingAddress;
+    }
+
+    public Address getMailingAddress() {
+        return mailingAddress;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 
 
 }
