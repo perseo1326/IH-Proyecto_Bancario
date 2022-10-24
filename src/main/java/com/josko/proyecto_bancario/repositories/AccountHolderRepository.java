d91ba48bb20d49b3d991089dde1e90a8f3328399
John W <perseo1326@gmail.com>
2022-10-24 18:14:00 +0200

    Creacion de repositorio 'AdminRepository' #9

--- src/main/java/com/josko/proyecto_bancario/repositories/AccountHolderRepository.java
+++ src/main/java/com/josko/proyecto_bancario/repositories/AccountHolderRepository.java
@@ -1,11 +1,11 @@
 package com.josko.proyecto_bancario.repositories;
 
+import com.josko.proyecto_bancario.models.AccountHolder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccountHolder extends JpaRepository<AccountHolder, Long> {
-
+public interface AccountHolderRepository extends JpaRepository<AccountHolder, Long> {
 
 
 
--- src/main/java/com/josko/proyecto_bancario/repositories/AdminRepository.java
+++ src/main/java/com/josko/proyecto_bancario/repositories/AdminRepository.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 public interface AdminRepository extends JpaRepository<Admin, Long> {
 
 
+
+
 }
