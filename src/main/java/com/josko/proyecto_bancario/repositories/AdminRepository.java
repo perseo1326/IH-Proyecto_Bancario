cf3213cb46f807eed8e9f44d88a529305f576ee3
John W <perseo1326@gmail.com>
2022-10-24 18:05:30 +0200

    Creacion de repositorio 'AdminRepository'

---
+++ src/main/java/com/josko/proyecto_bancario/repositories/AdminRepository.java
@@ -0,0 +1,11 @@
+package com.josko.proyecto_bancario.repositories;
+
+import com.josko.proyecto_bancario.models.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+
+
+}
