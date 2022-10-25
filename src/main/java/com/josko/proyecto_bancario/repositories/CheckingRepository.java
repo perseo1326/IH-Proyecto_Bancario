cb1dda44e11fd967ded50043eabd86a27aa30036
John W <perseo1326@gmail.com>
2022-10-26 00:41:05 +0200

    actualizacion del repositorio 'CheckingRepository'. #19

--- src/main/java/com/josko/proyecto_bancario/repositories/CheckingRepository.java
+++ src/main/java/com/josko/proyecto_bancario/repositories/CheckingRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Checking extends JpaRepository<Checking, Long> {
+public interface CheckingRepository extends JpaRepository<CheckingRepository, Long> {
 
 
 
