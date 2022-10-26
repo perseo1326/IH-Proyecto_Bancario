3e413d9e64746641f0a5a4d856004bcac2bfddcc
John W <perseo1326@gmail.com>
2022-10-26 13:43:36 +0200

    Adicion del metodo 'isSecretKeyValid' para validar una secret key que sea numerica de 4 digitos. #24

--- src/main/java/com/josko/proyecto_bancario/services/ValidatorService.java
+++ src/main/java/com/josko/proyecto_bancario/services/ValidatorService.java
@@ -1,11 +1,18 @@
 package com.josko.proyecto_bancario.services;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ValidatorService {
 
+    public boolean isSecretKeyValid(String secretKey) {
 
+        if (!secretKey.matches("[0-9]{4}"))
+            throw new IllegalArgumentException("Not a valid hashed key supplied.");
+        return true;
+    }
 
 
 }
