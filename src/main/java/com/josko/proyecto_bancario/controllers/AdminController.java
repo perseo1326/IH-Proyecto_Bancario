6686b4f6a0ac67470f68c721180e9a75a57f1b91
John W <perseo1326@gmail.com>
2022-10-26 13:56:52 +0200

    Creacion endpoint para creacion de 'ThirdParty' users en la DB #25

--- src/main/java/com/josko/proyecto_bancario/controllers/AdminController.java
+++ src/main/java/com/josko/proyecto_bancario/controllers/AdminController.java
@@ -1,12 +1,20 @@
 package com.josko.proyecto_bancario.controllers;
 
+import com.josko.proyecto_bancario.DTOs.ThirdPartyDTO;
+import com.josko.proyecto_bancario.models.ThirdParty;
+import com.josko.proyecto_bancario.services.AdminService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/api/v1/")
+@RequiredArgsConstructor
 public class AdminController {
 
+    private final AdminService adminService;
 
     @RequestMapping("/admins")
     @ResponseStatus(HttpStatus.OK)
@@ -17,8 +25,9 @@ public class AdminController {
 
     @PostMapping("/admins")
     @ResponseStatus(HttpStatus.CREATED)
-    public String createThirdPartyUser(@RequestBody ) {
+    public ThirdParty createThirdPartyUser(@Valid @RequestBody ThirdPartyDTO newThirdPartyDTO) {
 
+        return adminService.createThirdPartyUser(newThirdPartyDTO);
 
     }
 }
