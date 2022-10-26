bebd41c544fab6f509310a3974ac251b90dd4d6c
John W <perseo1326@gmail.com>
2022-10-26 17:53:44 +0200

    restriccion de 'interesRate' y 'minimumBalance' #16

--- src/main/java/com/josko/proyecto_bancario/models/Savings.java
+++ src/main/java/com/josko/proyecto_bancario/models/Savings.java
@@ -31,11 +31,21 @@ public class Savings extends Account {
     public Savings(String iban, LocalDate creationDate, BigDecimal penaltyFee, AccountHolder firstAccountHolder, Optional<AccountHolder> secondAccountholder, BigDecimal interesRate, BigDecimal minimumBalance) {
         super(iban, creationDate, penaltyFee, firstAccountHolder, secondAccountholder);
 
+        /*
+            RESTRICTION:
+                - Savings accounts have a default interest rate of 0.0025 ( 0.25%)
+                - Savings accounts may be instantiated with an interest rate other than the default, with a maximum interest rate of 0.5 (0.25% < X < 0.5%)
+         */
         if ((interesRate.compareTo(new BigDecimal(0.0025)) < 0) || interesRate.compareTo(new BigDecimal(0.005)) > 0 ) {
             log.error("PERSISTENCE:SAVINGS:Constructor: The interes rate is not in a valid range.");
             throw new IllegalArgumentException("The interes rate do not meet the request.");
         }
 
+        /*
+            RESTRICTION:
+                - Savings accounts should have a default minimumBalance of 1000 (X > 1K)
+                - Savings accounts may be instantiated with a minimum balance of less than 1000 but no lower than 100 ( 100 < X < 1K)
+         */
         if (minimumBalance.compareTo(new BigDecimal(100)) < 0 || minimumBalance.compareTo(new BigDecimal(1000)) > 0 ) {
             log.error("PERSISTENCE:SAVINGS:Constructor: The minimumBalance is not valid (" + minimumBalance.toString() + ").");
             throw new IllegalArgumentException("The minimumBalance is not in a valid range.");
