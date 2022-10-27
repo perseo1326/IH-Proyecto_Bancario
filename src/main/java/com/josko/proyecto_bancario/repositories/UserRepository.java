4582c38e772094be0ffb96cb42cca825e4dc57a4
John W <perseo1326@gmail.com>
2022-10-27 18:42:27 +0200

    Creacion de Query para busquedas de usuario por su nombre #30

--- src/main/java/com/josko/proyecto_bancario/repositories/UserRepository.java
+++ src/main/java/com/josko/proyecto_bancario/repositories/UserRepository.java
@@ -2,10 +2,17 @@ package com.josko.proyecto_bancario.repositories;
 
 import com.josko.proyecto_bancario.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("select u from User u where upper(u.name) like upper(concat('%', :name, '%')) order by u.name")
+    List<User> findByNameContainsOrderByName(@Param("name") String name);
+
 
 
 }
