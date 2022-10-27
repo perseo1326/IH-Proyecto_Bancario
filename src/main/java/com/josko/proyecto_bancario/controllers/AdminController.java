c7086b8b1467f5ff8386a69c1b6e74d91725f2c7
John W <perseo1326@gmail.com>
2022-10-27 20:25:39 +0200

    REQUIREMENT: GET all accountHolders from de DB. #32

--- src/main/java/com/josko/proyecto_bancario/controllers/AdminController.java
+++ src/main/java/com/josko/proyecto_bancario/controllers/AdminController.java
@@ -1,13 +1,19 @@
 package com.josko.proyecto_bancario.controllers;
 
 import com.josko.proyecto_bancario.DTOs.ThirdPartyDTO;
+import com.josko.proyecto_bancario.models.AccountHolder;
+import com.josko.proyecto_bancario.models.BasicAccount;
 import com.josko.proyecto_bancario.models.ThirdParty;
+import com.josko.proyecto_bancario.models.User;
 import com.josko.proyecto_bancario.services.AdminService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/")
@@ -18,7 +24,7 @@ public class AdminController {
 
     @RequestMapping("/admins")
     @ResponseStatus(HttpStatus.OK)
-    public String pruebaHello() {
+    public String adminWelcome() {
 
         return "Hola, Bienvenido a la interfaz bancaria.";
     }
@@ -28,6 +34,37 @@ public class AdminController {
     public ThirdParty createThirdPartyUser(@Valid @RequestBody ThirdPartyDTO newThirdPartyDTO) {
 
         return adminService.createThirdPartyUser(newThirdPartyDTO);
-
     }
+
+    /*
+        1. Obtener el listado de todos los accountholders
+     */
+    @GetMapping("/admins/accountholders")
+    @ResponseStatus(HttpStatus.OK)
+//    public List<AccountHolder> getAccountHolders(@PathVariable("id") Optional<Long> accountHolderId, @RequestParam("name") Optional<String> userName) {
+    public List<AccountHolder> getAccountHolders() {
+/*
+        if (accountHolderId.isPresent()) {
+            return adminService.getAccountHolderById(accountHolderId.get());
+        }
+
+        if(userName.isPresent()) {
+//            return adminService.getAccountHolderByName(userName.get());
+        }
+*/
+        return adminService.findAllAccountHolders();
+    }
+
+    @PostMapping("/admins/accounts")
+    @ResponseStatus(HttpStatus.CREATED)
+    public BasicAccount createNewAccount(@RequestBody Map<String, String> values) {
+
+        return adminService.createNewAccount(values);
+    }
+
+
+
+
+
+
 }
--- src/main/java/com/josko/proyecto_bancario/services/AdminService.java
+++ src/main/java/com/josko/proyecto_bancario/services/AdminService.java
@@ -1,11 +1,20 @@
 package com.josko.proyecto_bancario.services;
 
 import com.josko.proyecto_bancario.DTOs.ThirdPartyDTO;
-import com.josko.proyecto_bancario.models.ThirdParty;
+import com.josko.proyecto_bancario.enums.AccountTypeEnum;
+import com.josko.proyecto_bancario.exeptions.IdNotValidExeption;
+import com.josko.proyecto_bancario.models.*;
+import com.josko.proyecto_bancario.repositories.AccountHolderRepository;
 import com.josko.proyecto_bancario.repositories.ThirdPartyReposiyory;
+import com.josko.proyecto_bancario.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -13,12 +22,81 @@ import org.springframework.stereotype.Service;
 public class AdminService {
 
     private final ThirdPartyReposiyory thirdPartyReposiyory;
+    private final UserRepository userRepository;
+    private final AccountHolderRepository accountHolderRepository;
 
+    // CREATE ThirdParty entities
     public ThirdParty createThirdPartyUser(ThirdPartyDTO newThirdPartyDTO) {
         log.info("SERVICE:ADMINSERVICE:createThirdPartyUser: Creating a new ThirdParty user.");
 
         ThirdParty thirdPartyUser = new ThirdParty(newThirdPartyDTO.getName(),newThirdPartyDTO.getHashedKey());
         return thirdPartyReposiyory.save((thirdPartyUser));
+    }
+
+    // GET AccountHolders By ID
+    public List<AccountHolder> getAccountHolderById(Long userId) {
+
+        /*
+        Optional<User> user = userRepository.findById(userId);
+        if (user.isEmpty()) {
+            log.warn("SERVICE:ADMINSERVICE:getAccountHolderById: The given ID(" + userId.toString() + ") was not found.");
+            throw new IdNotValidExeption(userId.toString());
+        }
+
+        List<User> users = new ArrayList<User>();
+        users.add(user.get());
+//        return users;
+
+         */
+        return null;
+    }
+
+    // GET AccountHolders By name
+    public List<User> getAccountHolderByName(String name) {
+
+        /*
+        List<User> users = userRepository.findByNameContainsOrderByName(name);
+        if (users.isEmpty()) {
+            log.warn("SERVICE:ADMINSERVICE:getAccountHolderByName: The given name(" + name + ") was not found.");
+            throw new IdNotValidExeption(name);
+        }
+        return users;
+
+         */
+        return null;
+    }
+
+    public List<AccountHolder> findAllAccountHolders() {
+
+        return accountHolderRepository.findAll();
+    }
+
+    public BasicAccount createNewAccount(Map<String, String> values) {
+/*
+        if (!values.containsKey("accountType") || !AccountTypeEnum.CHECKING.contains(values.get("accountType"))) {
+            log.warn("SERVICE:ADMINSERVICE:createNewAccount: No valid accountType selected.");
+            throw new IllegalArgumentException("No valid account type selected.");
+        }
+
+        if (!values.containsKey("mainOwnerId")) {
+            log.warn("SERVICE:ADMINSERVICE:createNewAccount: The values supplied are not valid.");
+            throw new IllegalArgumentException("The values supplied are not valid.");
+        }
+
+        Optional<User> mainOwner = userRepository.findById(Long.parseLong(values.get("mainOwnerId")));
+
+        if (mainOwner.isEmpty()) {
+            log.warn("SERVICE:ADMINSERVICE:createNewAccount: Not a valid user ID provided.");
+            throw new IllegalArgumentException("Not a valid user ID provided.");
+        }
+
+        // TODO: necesario terminar
+
+
+        System.out.println("\n" + values);
+  */
+        return null;
+
     }
 
 
