b1e2994bc66856684ce757e88e44de20bbfd81db
John W <perseo1326@gmail.com>
2022-10-28 23:38:07 +0200

    Modified class, removed the statement 'abstract' #3

--- src/main/java/com/josko/proyecto_bancario/models/User.java
+++ src/main/java/com/josko/proyecto_bancario/models/User.java
@@ -7,7 +7,7 @@ import java.util.Objects;
 @Entity
 @Table (name = "users")
 @Inheritance(strategy = InheritanceType.JOINED)
-public abstract class User {
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
