b393e7cac6f21ff9e551243060a12184c84b0176
John W <perseo1326@gmail.com>
2022-10-29 23:47:18 +0200

    Validations of class 'CreditCard' were moved to 'ValidatorService' #24

--- src/main/java/com/josko/proyecto_bancario/models/CreditCard.java
+++ src/main/java/com/josko/proyecto_bancario/models/CreditCard.java
@@ -1,63 +1,39 @@
 package com.josko.proyecto_bancario.models;
 
+import com.josko.proyecto_bancario.services.ValidatorService;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.util.Optional;
 
 @Entity
 @Table(name = "credit_card")
 public class CreditCard extends BasicAccount {
 
+    @Transient
+    @Autowired
+    private ValidatorService validatorService;
+
     private BigDecimal creditLimit;
 
     private BigDecimal interesRate;
 
     public CreditCard() {
-        this.initCreditCard();
     }
 
-    public CreditCard(AccountHolder firstAccountHolder,
-                      Optional<AccountHolder> secondAccountholder,
-                      String iban,
-                      Money balance) {
-        super(firstAccountHolder, secondAccountholder, iban, balance);
-        this.initCreditCard();
-    }
-
-    /*
-        RESTRICTIONS:
-            1. CreditCard accounts have a default creditLimit of 100 ( X < 100 )
-            2. CreditCards may be instantiated with a creditLimit higher than 100 but not higher than 100K (100 < X 100K).
-            3. CreditCards have a default interestRate of 0.2 (20%)
-            4. CreditCards may be instantiated with an interestRate less than 0.2 but not lower than 0.1(10% < X < 20%)
-     */
     public CreditCard(AccountHolder firstAccountHolder,
                       Optional<AccountHolder> secondAccountholder,
                       String iban,
                       Money balance,
-                      BigDecimal creditLimit, BigDecimal interesRate) {
+                      Optional<BigDecimal> creditLimit,
+                      Optional<BigDecimal> interesRate) {
         super(firstAccountHolder, secondAccountholder, iban, balance);
-        // if credit limit fit -> 100 < X < 100K
 
-        if (creditLimit.compareTo(new BigDecimal(100)) < 0 || creditLimit.compareTo(new BigDecimal(100_000)) > 0 ) {
-            throw new IllegalArgumentException("The credit limit is not valid.");
-        }
-
-        if (interesRate.compareTo(new BigDecimal(0.1)) < 0 || interesRate.compareTo(new BigDecimal(0.2)) > 0) {
-            throw new IllegalArgumentException("The interes rate is not valid.");
-        }
-
-        this.creditLimit = creditLimit;
-        this.interesRate = interesRate;
-    }
-
-    private void initCreditCard() {
-        // default credit limit = 100
-        this.creditLimit = new BigDecimal(100);
-        // default interes rate = 20%
-        this.interesRate = new BigDecimal(0.2);
+        this.creditLimit = validatorService.validateCreditLimit(creditLimit);
+        this.interesRate = validatorService.validateCreditCartInteresRate(interesRate);
     }
 
     public BigDecimal getCreditLimit() {
--- src/main/java/com/josko/proyecto_bancario/services/ValidatorService.java
+++ src/main/java/com/josko/proyecto_bancario/services/ValidatorService.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.Transient;
 import java.math.BigDecimal;
 import java.util.Optional;
 
@@ -17,10 +16,13 @@ import java.util.Optional;
 public class ValidatorService {
 
     // default interes rate = 0.25% = 0.0025
-    private final BigDecimal INTERES_RATE = new BigDecimal(0.0025);
+    private final BigDecimal SAVINGS_INTERES_RATE = new BigDecimal(0.0025);
     // default minimumBalance = 1K
     private final BigDecimal MINIMUM_BALANCE = new BigDecimal(1000);
-
+    // CreditCard accounts have a default creditLimit of 100
+    private final BigDecimal CREDIT_LIMIT = new BigDecimal(100);
+    //  CreditCards have a default interestRate of 0.2
+    private final BigDecimal CREDIT_CARD_INTERES_RATE = new BigDecimal(0.2);
     private final AccountHolderRepository accountHolderRepository;
 
 
@@ -73,7 +75,7 @@ public class ValidatorService {
         BigDecimal validInteresRate;
 
         if (interesRate.isEmpty()) {
-            validInteresRate = INTERES_RATE;
+            validInteresRate = SAVINGS_INTERES_RATE;
         } else {
             /*
                 RESTRICTION:
@@ -112,6 +114,54 @@ public class ValidatorService {
             validMinimumBalance = minimumBalance.get();
         }
         return validMinimumBalance;
+    }
+
+    /*
+        RESTRICTIONS to validate:
+            1. CreditCard accounts have a default creditLimit of 100 ( X < 100 )
+            2. CreditCards may be instantiated with a creditLimit higher than 100 but not higher than 100K (100 < X 100K).
+    */
+    public BigDecimal validateCreditLimit(Optional<BigDecimal> creditLimit) {
+        log.info("VALIDATORSERVICE:validateCreditLimit: Validating the Credit Limit.");
+
+        BigDecimal validCreditLimit;
+
+        // if credit limit fit -> 100 < X < 100K
+
+        if (creditLimit.isEmpty()) {
+            validCreditLimit = CREDIT_LIMIT;
+        } else {
+            if (creditLimit.get().compareTo(new BigDecimal(100)) < 0 || creditLimit.get().compareTo(new BigDecimal(100_000)) > 0) {
+                log.warn("VALIDATORSERVICE:validateCreditLimit: the creditLimit value supplied is not in a valid range.");
+                throw new IllegalArgumentException("The credit limit is not valid.");
+            }
+            validCreditLimit = creditLimit.get();
+        }
+        return validCreditLimit;
+    }
+
+    /*
+        RESTRICTIONS to validate:
+            3. CreditCards have a default interestRate of 0.2 (20%)
+            4. CreditCards may be instantiated with an interestRate less than 0.2 but not lower than 0.1(10% < X < 20%)
+     */
+
+    public BigDecimal validateCreditCartInteresRate(Optional<BigDecimal> creditCardInteresRate) {
+        log.info("VALIDATORSERVICE:validateCreditCartInteresRate: Validating Credit Card Interes Rate is valid.");
+
+        BigDecimal validCreditCartInteresRate;
+
+        if (creditCardInteresRate.isEmpty()) {
+            validCreditCartInteresRate = CREDIT_CARD_INTERES_RATE;
+        }
+        else {
+            if (creditCardInteresRate.get().compareTo(new BigDecimal(0.1)) < 0 || creditCardInteresRate.get().compareTo(new BigDecimal(0.2)) > 0) {
+                log.warn("VALIDATORSERVICE:validateCreditCartInteresRate: The Credit Card Interes Rate is in a not valid range.");
+                throw new IllegalArgumentException("The interes rate is not in a valid range.");
+            }
+            validCreditCartInteresRate = creditCardInteresRate.get();
+        }
+        return validCreditCartInteresRate;
     }
 
 
