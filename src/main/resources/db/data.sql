6c931ef6c497270f7f830f8d0e5bf80972e8f366
John W <perseo1326@gmail.com>
2022-10-30 23:28:18 +0100

    Added valid passwords for all users (pass = 123456) Added data for Roles for user: Admin, AccountHolders and ThirdParty. #12

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,6 +1,7 @@
 
 
 DELETE FROM roles WHERE TRUE;
+DELETE FROM user_roles WHERE TRUE;
 DELETE FROM admins WHERE TRUE;
 DELETE FROM third_parties WHERE TRUE;
 DELETE FROM account_holders WHERE TRUE;
@@ -9,9 +10,9 @@ DELETE FROM addresses WHERE TRUE;
 
 DELETE FROM users WHERE TRUE;
 
-INSERT INTO roles (role_name) VALUES ('ROLE_USER'),
-                                ('ROLE_MODERATOR'),
-                                ('ROLE_ADMIN');
+INSERT INTO roles (role_name) VALUES ('ROLE_ADMIN'),
+                                ('ROLE_USER'),
+                                ('ROLE_THIRDPARTY');
 
 -- Insercion de direcciones
 INSERT INTO addresses (country, city, street, house_number, home_unit, COMMENT)   VALUES ('España', 'Barcelona', 'Numancia', '23', '4-2', '' );
@@ -27,39 +28,39 @@ INSERT INTO addresses (country, city, street, house_number, home_unit, COMMENT) 
 INSERT INTO addresses (country, city, street, house_number, home_unit, COMMENT)   VALUES ('Mexico', 'Merida', 'Paseo de Montejo', '33', 'Casa Azul', 'Al lado del monumento' );
 
 -- insercion de usuarios Administradores
-INSERT INTO users (name, username, email, password) VALUES ('John', 'john', 'john@email.com', '123456');
-INSERT INTO users (name, username, email, password) VALUES ('Alfredo', 'alfredo', 'alfredo@email.com', '123456');
-INSERT INTO users (name, username, email, password) VALUES ('Cristian', 'cristian', 'cristian@email.com', '123456');
-INSERT INTO users (name, username, email, password) VALUES ('Raúl', 'raúl', 'raúl@email.com', '123456');
+INSERT INTO users (name, username, email, password) VALUES ('John', 'john', 'john@email.com', '$2a$10$EmDCvndz9V.qV4s48TCXYOplNJc54si3hM2yby.stIO3ETgMoWFbS');
+INSERT INTO users (name, username, email, password) VALUES ('Alfredo', 'alfredo', 'alfredo@email.com', '$2a$10$EmDCvndz9V.qV4s48TCXYOplNJc54si3hM2yby.stIO3ETgMoWFbS');
+INSERT INTO users (name, username, email, password) VALUES ('Cristian', 'cristian', 'cristian@email.com', '$2a$10$EmDCvndz9V.qV4s48TCXYOplNJc54si3hM2yby.stIO3ETgMoWFbS');
+INSERT INTO users (name, username, email, password) VALUES ('Raúl', 'raúl', 'raúl@email.com', '$2a$10$EmDCvndz9V.qV4s48TCXYOplNJc54si3hM2yby.stIO3ETgMoWFbS');
 
 
 -- insercion de usuarios 'Users'
-INSERT INTO users (name, username, email, password) VALUES ('Alberto', 'alberto', 'alberto@email.com', '123456');
-INSERT INTO users (name, username, email, password) VALUES ('Bernardo', 'bernardo', 'bernardo@email.com', '123456');
-INSERT INTO users (name, username, email, password) VALUES ('Carlos', 'carlos', 'carlos@email.com', '123456');
-INSERT INTO users (name, username, email, password) VALUES ('Daniel', 'daniel', 'daniel@email.com', '123456');
-INSERT INTO users (name, username, email, password) VALUES ('Ernesto', 'ernesto', 'ernesto@email.com', '123456');
-INSERT INTO users (name, username, email, password) VALUES ('Fernando', 'fernando', 'fernando@email.com', '123456');
-INSERT INTO users (name, username, email, password) VALUES ('Gladys', 'gladys', 'gladys@email.com', '123456');
-INSERT INTO users (name, username, email, password) VALUES ('Hernan', 'hernan', 'hernan@email.com', '123456');
-INSERT INTO users (name, username, email, password) VALUES ('Ivan', 'ivan', 'ivan@email.com', '123456');
-INSERT INTO users (name, username, email, password) VALUES ('Jose', 'jose', 'jose@email.com', '123456');
-INSERT INTO users (name, username, email, password) VALUES ('Kevin', 'kevin', 'kevin@email.com', '123456');
-INSERT INTO users (name, username, email, password) VALUES ('Liliana', 'liliana', 'liliana@email.com', '123456');
-INSERT INTO users (name, username, email, password) VALUES ('Maria', 'maria', 'maria@email.com', '123456');
-INSERT INTO users (name, username, email, password) VALUES ('Nancy', 'nancy', 'nancy@email.com', '123456');
-INSERT INTO users (name, username, email, password) VALUES ('Orlando', 'orlando', 'orlando@email.com', '123456');
-INSERT INTO users (name, username, email, password) VALUES ('Pedro', 'pedro', 'pedro@email.com', '123456');
-INSERT INTO users (name, username, email, password) VALUES ('Quentin', 'quentin', 'quentin@email.com', '123456');
-INSERT INTO users (name, username, email, password) VALUES ('Ramon', 'ramon', 'ramon@email.com', '123456');
-INSERT INTO users (name, username, email, password) VALUES ('Samuel', 'samuel', 'samuel@email.com', '123456');
-INSERT INTO users (name, username, email, password) VALUES ('Tomas', 'tomas', 'tomas@email.com', '123456');
-INSERT INTO users (name, username, email, password) VALUES ('Ubaldo', 'ubaldo', 'ubaldo@email.com', '123456');
-INSERT INTO users (name, username, email, password) VALUES ('Veronica', 'veronica', 'veronica@email.com', '123456');
-INSERT INTO users (name, username, email, password) VALUES ('Wendy', 'wendy', 'wendy@email.com', '123456');
-INSERT INTO users (name, username, email, password) VALUES ('Xochil', 'xochil', 'xochil@email.com', '123456');
-INSERT INTO users (name, username, email, password) VALUES ('Yolanda', 'yolanda', 'yolanda@email.com', '123456');
-INSERT INTO users (name, username, email, password) VALUES ('Zoe', 'zoe', 'zoe@email.com', '123456');
+INSERT INTO users (name, username, email, password) VALUES ('Alberto', 'alberto', 'alberto@email.com', '$2a$10$5VmIyfjdIhdb6GKfIkxwSelOCbI516ZxAfOJic21gHk.KnWE8N.gK');
+INSERT INTO users (name, username, email, password) VALUES ('Bernardo', 'bernardo', 'bernardo@email.com', '$2a$10$5VmIyfjdIhdb6GKfIkxwSelOCbI516ZxAfOJic21gHk.KnWE8N.gK');
+INSERT INTO users (name, username, email, password) VALUES ('Carlos', 'carlos', 'carlos@email.com', '$2a$10$5VmIyfjdIhdb6GKfIkxwSelOCbI516ZxAfOJic21gHk.KnWE8N.gK');
+INSERT INTO users (name, username, email, password) VALUES ('Daniel', 'daniel', 'daniel@email.com', '$2a$10$5VmIyfjdIhdb6GKfIkxwSelOCbI516ZxAfOJic21gHk.KnWE8N.gK');
+INSERT INTO users (name, username, email, password) VALUES ('Ernesto', 'ernesto', 'ernesto@email.com', '$2a$10$5VmIyfjdIhdb6GKfIkxwSelOCbI516ZxAfOJic21gHk.KnWE8N.gK');
+INSERT INTO users (name, username, email, password) VALUES ('Fernando', 'fernando', 'fernando@email.com', '$2a$10$5VmIyfjdIhdb6GKfIkxwSelOCbI516ZxAfOJic21gHk.KnWE8N.gK');
+INSERT INTO users (name, username, email, password) VALUES ('Gladys', 'gladys', 'gladys@email.com', '$2a$10$5VmIyfjdIhdb6GKfIkxwSelOCbI516ZxAfOJic21gHk.KnWE8N.gK');
+INSERT INTO users (name, username, email, password) VALUES ('Hernan', 'hernan', 'hernan@email.com', '$2a$10$5VmIyfjdIhdb6GKfIkxwSelOCbI516ZxAfOJic21gHk.KnWE8N.gK');
+INSERT INTO users (name, username, email, password) VALUES ('Ivan', 'ivan', 'ivan@email.com', '$2a$10$5VmIyfjdIhdb6GKfIkxwSelOCbI516ZxAfOJic21gHk.KnWE8N.gK');
+INSERT INTO users (name, username, email, password) VALUES ('Jose', 'jose', 'jose@email.com', '$2a$10$5VmIyfjdIhdb6GKfIkxwSelOCbI516ZxAfOJic21gHk.KnWE8N.gK');
+INSERT INTO users (name, username, email, password) VALUES ('Kevin', 'kevin', 'kevin@email.com', '$2a$10$5VmIyfjdIhdb6GKfIkxwSelOCbI516ZxAfOJic21gHk.KnWE8N.gK');
+INSERT INTO users (name, username, email, password) VALUES ('Liliana', 'liliana', 'liliana@email.com', '$2a$10$5VmIyfjdIhdb6GKfIkxwSelOCbI516ZxAfOJic21gHk.KnWE8N.gK');
+INSERT INTO users (name, username, email, password) VALUES ('Maria', 'maria', 'maria@email.com', '$2a$10$5VmIyfjdIhdb6GKfIkxwSelOCbI516ZxAfOJic21gHk.KnWE8N.gK');
+INSERT INTO users (name, username, email, password) VALUES ('Nancy', 'nancy', 'nancy@email.com', '$2a$10$5VmIyfjdIhdb6GKfIkxwSelOCbI516ZxAfOJic21gHk.KnWE8N.gK');
+INSERT INTO users (name, username, email, password) VALUES ('Orlando', 'orlando', 'orlando@email.com', '$2a$10$5VmIyfjdIhdb6GKfIkxwSelOCbI516ZxAfOJic21gHk.KnWE8N.gK');
+INSERT INTO users (name, username, email, password) VALUES ('Pedro', 'pedro', 'pedro@email.com', '$2a$10$5VmIyfjdIhdb6GKfIkxwSelOCbI516ZxAfOJic21gHk.KnWE8N.gK');
+INSERT INTO users (name, username, email, password) VALUES ('Quentin', 'quentin', 'quentin@email.com', '$2a$10$5VmIyfjdIhdb6GKfIkxwSelOCbI516ZxAfOJic21gHk.KnWE8N.gK');
+INSERT INTO users (name, username, email, password) VALUES ('Ramon', 'ramon', 'ramon@email.com', '$2a$10$5VmIyfjdIhdb6GKfIkxwSelOCbI516ZxAfOJic21gHk.KnWE8N.gK');
+INSERT INTO users (name, username, email, password) VALUES ('Samuel', 'samuel', 'samuel@email.com', '$2a$10$5VmIyfjdIhdb6GKfIkxwSelOCbI516ZxAfOJic21gHk.KnWE8N.gK');
+INSERT INTO users (name, username, email, password) VALUES ('Tomas', 'tomas', 'tomas@email.com', '$2a$10$5VmIyfjdIhdb6GKfIkxwSelOCbI516ZxAfOJic21gHk.KnWE8N.gK');
+INSERT INTO users (name, username, email, password) VALUES ('Ubaldo', 'ubaldo', 'ubaldo@email.com', '$2a$10$5VmIyfjdIhdb6GKfIkxwSelOCbI516ZxAfOJic21gHk.KnWE8N.gK');
+INSERT INTO users (name, username, email, password) VALUES ('Veronica', 'veronica', 'veronica@email.com', '$2a$10$5VmIyfjdIhdb6GKfIkxwSelOCbI516ZxAfOJic21gHk.KnWE8N.gK');
+INSERT INTO users (name, username, email, password) VALUES ('Wendy', 'wendy', 'wendy@email.com', '$2a$10$5VmIyfjdIhdb6GKfIkxwSelOCbI516ZxAfOJic21gHk.KnWE8N.gK');
+INSERT INTO users (name, username, email, password) VALUES ('Xochil', 'xochil', 'xochil@email.com', '$2a$10$5VmIyfjdIhdb6GKfIkxwSelOCbI516ZxAfOJic21gHk.KnWE8N.gK');
+INSERT INTO users (name, username, email, password) VALUES ('Yolanda', 'yolanda', 'yolanda@email.com', '$2a$10$5VmIyfjdIhdb6GKfIkxwSelOCbI516ZxAfOJic21gHk.KnWE8N.gK');
+INSERT INTO users (name, username, email, password) VALUES ('Zoe', 'zoe', 'zoe@email.com', '$2a$10$5VmIyfjdIhdb6GKfIkxwSelOCbI516ZxAfOJic21gHk.KnWE8N.gK');
 
 
 -- insercion y enlace de administradores con usuarios
@@ -80,7 +81,6 @@ INSERT INTO third_parties (user_id, hashed_key) VALUES ((SELECT user_id FROM  us
 INSERT INTO third_parties (user_id, hashed_key) VALUES ((SELECT user_id FROM  users WHERE NAME LIKE 'Zoe') , 'zzzz');
 
 
-
 -- insercion y enlace de entidades 'AccountHolders'
 INSERT INTO account_holders (user_id, birth_date, main_address_address_id, secondary_address_address_id) VALUES ((SELECT user_id FROM  users WHERE NAME LIKE 'Alberto') , '1960-02-24', (SELECT address_id FROM addresses WHERE street LIKE 'Numancia' AND house_number LIKE '23') , null);
 INSERT INTO account_holders (user_id, birth_date, main_address_address_id, secondary_address_address_id) VALUES ((SELECT user_id FROM  users WHERE NAME LIKE 'Bernardo') , '1990-03-23', (SELECT address_id FROM addresses WHERE street LIKE 'Gran Via' AND house_number LIKE '50') , null);
@@ -95,3 +95,33 @@ INSERT INTO account_holders (user_id, birth_date, main_address_address_id, secon
 INSERT INTO account_holders (user_id, birth_date, main_address_address_id, secondary_address_address_id) VALUES ((SELECT user_id FROM  users WHERE NAME LIKE 'Kevin') , '1982-12-02', (SELECT address_id FROM addresses WHERE street LIKE 'Paseo de Montejo' AND house_number LIKE '33') , null);
 
 
+-- insercion en user_roles para enlazar administradores con sus roles de Admin
+INSERT INTO user_roles (user_id, role_id) VALUES(( (SELECT user_id FROM users WHERE NAME LIKE 'John')  ), ( SELECT ID FROM roles WHERE role_name LIKE 'ROLE_ADMIN' ) );
+INSERT INTO user_roles (user_id, role_id) VALUES(( (SELECT user_id FROM users WHERE NAME LIKE 'Alfredo')  ), ( SELECT ID FROM roles WHERE role_name LIKE 'ROLE_ADMIN' ) );
+INSERT INTO user_roles (user_id, role_id) VALUES(( (SELECT user_id FROM users WHERE NAME LIKE 'Cristian')  ), ( SELECT ID FROM roles WHERE role_name LIKE 'ROLE_ADMIN' ) );
+INSERT INTO user_roles (user_id, role_id) VALUES(( (SELECT user_id FROM users WHERE NAME LIKE 'Raúl')  ), ( SELECT ID FROM roles WHERE role_name LIKE 'ROLE_ADMIN' ) );
+
+
+-- insercion de Roles para usuarios 'AccountHolders'
+INSERT INTO user_roles (user_id, role_id) VALUES( (SELECT user_id FROM users WHERE NAME LIKE 'Alberto'), ( SELECT ID FROM roles WHERE role_name LIKE 'ROLE_USER') );
+INSERT INTO user_roles (user_id, role_id) VALUES( (SELECT user_id FROM users WHERE NAME LIKE 'Bernardo'), ( SELECT ID FROM roles WHERE role_name LIKE 'ROLE_USER') );
+INSERT INTO user_roles (user_id, role_id) VALUES( (SELECT user_id FROM users WHERE NAME LIKE 'Carlos'), ( SELECT ID FROM roles WHERE role_name LIKE 'ROLE_USER') );
+INSERT INTO user_roles (user_id, role_id) VALUES( (SELECT user_id FROM users WHERE NAME LIKE 'Daniel'), ( SELECT ID FROM roles WHERE role_name LIKE 'ROLE_USER') );
+INSERT INTO user_roles (user_id, role_id) VALUES( (SELECT user_id FROM users WHERE NAME LIKE 'Ernesto'), ( SELECT ID FROM roles WHERE role_name LIKE 'ROLE_USER') );
+INSERT INTO user_roles (user_id, role_id) VALUES( (SELECT user_id FROM users WHERE NAME LIKE 'Fernando'), ( SELECT ID FROM roles WHERE role_name LIKE 'ROLE_USER') );
+INSERT INTO user_roles (user_id, role_id) VALUES( (SELECT user_id FROM users WHERE NAME LIKE 'Gladys'), ( SELECT ID FROM roles WHERE role_name LIKE 'ROLE_USER') );
+INSERT INTO user_roles (user_id, role_id) VALUES( (SELECT user_id FROM users WHERE NAME LIKE 'Hernan'), ( SELECT ID FROM roles WHERE role_name LIKE 'ROLE_USER') );
+INSERT INTO user_roles (user_id, role_id) VALUES( (SELECT user_id FROM users WHERE NAME LIKE 'Ivan'), ( SELECT ID FROM roles WHERE role_name LIKE 'ROLE_USER') );
+INSERT INTO user_roles (user_id, role_id) VALUES( (SELECT user_id FROM users WHERE NAME LIKE 'Jose'), ( SELECT ID FROM roles WHERE role_name LIKE 'ROLE_USER') );
+INSERT INTO user_roles (user_id, role_id) VALUES( (SELECT user_id FROM users WHERE NAME LIKE 'Kevin'), ( SELECT ID FROM roles WHERE role_name LIKE 'ROLE_USER') );
+
+
+-- insercion de Roles para usuarios 'ThirdParty'
+INSERT INTO user_roles (user_id, role_id) VALUES( (SELECT user_id FROM users WHERE NAME LIKE 'Samuel'), ( SELECT ID FROM roles WHERE role_name LIKE 'ROLE_THIRDPARTY') );
+INSERT INTO user_roles (user_id, role_id) VALUES( (SELECT user_id FROM users WHERE NAME LIKE 'Tomas'), ( SELECT ID FROM roles WHERE role_name LIKE 'ROLE_THIRDPARTY') );
+INSERT INTO user_roles (user_id, role_id) VALUES( (SELECT user_id FROM users WHERE NAME LIKE 'Ubaldo'), ( SELECT ID FROM roles WHERE role_name LIKE 'ROLE_THIRDPARTY') );
+INSERT INTO user_roles (user_id, role_id) VALUES( (SELECT user_id FROM users WHERE NAME LIKE 'Veronica'), ( SELECT ID FROM roles WHERE role_name LIKE 'ROLE_THIRDPARTY') );
+INSERT INTO user_roles (user_id, role_id) VALUES( (SELECT user_id FROM users WHERE NAME LIKE 'Wendy'), ( SELECT ID FROM roles WHERE role_name LIKE 'ROLE_THIRDPARTY') );
+INSERT INTO user_roles (user_id, role_id) VALUES( (SELECT user_id FROM users WHERE NAME LIKE 'Xochil'), ( SELECT ID FROM roles WHERE role_name LIKE 'ROLE_THIRDPARTY') );
+INSERT INTO user_roles (user_id, role_id) VALUES( (SELECT user_id FROM users WHERE NAME LIKE 'Yolanda'), ( SELECT ID FROM roles WHERE role_name LIKE 'ROLE_THIRDPARTY') );
+INSERT INTO user_roles (user_id, role_id) VALUES( (SELECT user_id FROM users WHERE NAME LIKE 'Zoe'), ( SELECT ID FROM roles WHERE role_name LIKE 'ROLE_THIRDPARTY') );
