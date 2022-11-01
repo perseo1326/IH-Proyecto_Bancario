f37e6e84e281d4cf9c69061aac0b0a833e229b17
John W <perseo1326@gmail.com>
2022-11-01 20:39:54 +0100

    Repository for 'BasicAccount' entities. #13

---
+++ src/main/java/com/josko/proyecto_bancario/repositories/BasicAccountRepository.java
@@ -0,0 +1,21 @@
+package com.josko.proyecto_bancario.repositories;
+
+import com.josko.proyecto_bancario.models.BasicAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BasicAccountRepository extends JpaRepository<BasicAccount, Long > {
+    @Query("""
+            select b from BasicAccount b
+            where b.firstAccountHolder.id = :id or b.secondAccountholder.id = :id1
+            order by b.firstAccountHolder.name""")
+    List<BasicAccount> findByFirstOrSecondAccountHolderById_OrderedByName(@Param("id") Long id, @Param("id1") Long id1);
+
+
+
+}
