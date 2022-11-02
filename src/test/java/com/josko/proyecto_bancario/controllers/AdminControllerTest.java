34855c631d6967e69adf9988be1f8480d72322b6
John W <perseo1326@gmail.com>
2022-11-02 17:03:54 +0100

    Class to manage integration test for the 'AdminController' #48

---
+++ src/test/java/com/josko/proyecto_bancario/controllers/AdminControllerTest.java
@@ -0,0 +1,66 @@
+package com.josko.proyecto_bancario.controllers;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class AdminControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void adminWelcome() {
+    }
+
+    @Test
+    void createThirdPartyUser() {
+    }
+
+    @Test
+    void createAccountHolderUser() {
+    }
+
+    @Test
+    void getAccountHoldersById() {
+    }
+
+    @Test
+    void getAccountHolders_AndByName() {
+    }
+
+    @Test
+    void getAccountHoldersByBirthDateRange() {
+    }
+
+    @Test
+    void createNewCheckingAccount() {
+
+    }
+
+    @Test
+    void createNewSavingsAccount() {
+    }
+
+    @Test
+    void createNewCreditCardAccount() {
+    }
+
+    @Test
+    void getAllAccountsFromAccountHolderUser() {
+    }
+
+    @Test
+    void getAccountFromAccountHolderByIban() {
+    }
+
+    @Test
+    void changeBalanceBasicInAccount() {
+    }
+}
