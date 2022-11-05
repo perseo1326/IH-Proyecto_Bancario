6a657fe5a181c715a57825c5d3703d825b267101
John W <perseo1326@gmail.com>
2022-11-05 01:25:46 +0100

    Added a new method to find 'thirdParty' User by his username. #10

--- src/main/java/com/josko/proyecto_bancario/repositories/ThirdPartyReposiyory.java
+++ src/main/java/com/josko/proyecto_bancario/repositories/ThirdPartyReposiyory.java
@@ -4,8 +4,11 @@ import com.josko.proyecto_bancario.models.ThirdParty;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ThirdPartyReposiyory extends JpaRepository<ThirdParty, Long> {
 
+    Optional<ThirdParty> findByUsernameIgnoreCase(String username);
 
 }
